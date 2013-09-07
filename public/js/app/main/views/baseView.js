388693ca85b1c1ace24158376fc4195e48d2e8f5
Paras Kuhad <paras.kuhad@changer.nl>
2013-09-08 04:43:27 +0530

    Experiments with twitter API

--- public/js/app/main/views/baseView.js
+++ public/js/app/main/views/baseView.js
@@ -2,16 +2,30 @@ define(['backbone', 'views/graphView'], function (Backbone, GraphView) {
     'use strict';
     var view = Backbone.View.extend({
         initialize: function (options) {
-            _.bindAll(this, 'render');
+            _.bindAll(this, 'render', 'renderTweet');
             this.socket = io.connect('http://localhost');
             this.socket.on('number', this.render);
-
+            this.socket.on('tweet', this.renderTweet);
             this.graph = new GraphView({el: this.$el.find('.graph-view')});
+
+            this.$number_counter = this.$el.find('.number-counter');
+            this.$tweet_board = this.$el.find('.tweet-board');
         },
 
         render: function(data){
-            this.$el.find('.number-counter').html('<h1>'+data.number+'</h1>');
+            this.$number_counter.html('<h1>'+data.number+'</h1>');
             this.graph.render(data.number);
+        },
+
+        renderTweet: function(data){
+            var that = this;
+            this.$tweet_board.html('');
+            console.log(data.tweet); 
+            _.each(data.tweet.statuses, function(tweet, index){
+                that.$tweet_board.append('<div class="tweet">'+tweet.text+'<b> '+tweet.user.followers_count+'</b></div>');
+                console.log(tweet);
+                console.log(tweet.text);
+            });
         }
     });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+// Initializing server
 server.listen(8888);
 
 app.use('/static', express.static(__dirname + '/public'));
@@ -10,14 +11,28 @@ app.get('/', function (req, res) {
   	res.sendfile(__dirname + '/index.html');
 });
 
+// Setting up Twitter
+var Twit = require('twit');
+
+var twit = new Twit({
+  consumer_key: 'Pneh19GTogq3kTxWq9dr9Q',
+  consumer_secret: '6umd0gXe3bz13GKGNg0ybfx6XlOhGyLp0IXgVEo',
+  access_token: '26528955-a97TOXHTchFrbmsjH2e8tKaScjbyN7EekHr3zo4ph',
+  access_token_secret: 'ffJR4aThNwg0knbQh9jZstNZCpg4HIsqTd2mgJE'
+});
+
 io.sockets.on('connection', function (socket) {
 
 	(function event_loop(){
 		setTimeout(function(){
-			var random_number = Math.floor(Math.random()*500)
-			socket.emit('number', { number: random_number });
-			event_loop();
-		}, 1000);
+			twit.get('search/tweets', { q: 'india', count: 100 }, function(err, reply) {
+				console.log(err);
+				var random_number = Math.floor(Math.random()*500)
+				socket.emit('number', { number: random_number });
+				socket.emit('tweet', { tweet: reply});
+				event_loop();
+			});
+		}, 5000);
 	})();
 
  	socket.on('my other event', function (data) {
