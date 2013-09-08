33fe24ada0fa9ab7345f05dcd217e073b148e658
Paras Kuhad <paras.kuhad@changer.nl>
2013-09-08 05:56:25 +0530

    Finally something working, a few clean ups and then will add feature

--- server.js
+++ server.js
@@ -7,38 +7,47 @@ var express = require('express')
 server.listen(8888);
 
 app.use('/static', express.static(__dirname + '/public'));
+var query = null;
 app.get('/', function (req, res) {
   	res.sendfile(__dirname + '/index.html');
+  	if(req.query.q){
+  		query = req.query.q;
+  	}
+  	console.log(query);
+
+  	// Setting up Twitter
+	var Twit = require('twit');
+
+	var twit = new Twit({
+	  consumer_key: 'Pneh19GTogq3kTxWq9dr9Q',
+	  consumer_secret: '6umd0gXe3bz13GKGNg0ybfx6XlOhGyLp0IXgVEo',
+	  access_token: '26528955-a97TOXHTchFrbmsjH2e8tKaScjbyN7EekHr3zo4ph',
+	  access_token_secret: 'ffJR4aThNwg0knbQh9jZstNZCpg4HIsqTd2mgJE'
+	});
+
+	//
+	io.sockets.on('connection', function (socket) {
+		(function event_loop(){
+			setTimeout(function(){
+				var quest = query || 'india';
+				twit.get('search/tweets', { q: quest, count: 100 }, function(err, reply) {
+					console.log(query);
+					console.log(err);
+					//var random_number = Math.floor(Math.random()*500)
+					//socket.emit('number', { number: random_number });
+					socket.emit('tweet', { tweet: reply});
+					//event_loop();
+				});
+			}, 10);
+		})();
+
+	 	socket.on('my other event', function (data) {
+	    	console.log(data);
+	  	});
+
+	});
 });
 
-// Setting up Twitter
-var Twit = require('twit');
 
-var twit = new Twit({
-  consumer_key: 'Pneh19GTogq3kTxWq9dr9Q',
-  consumer_secret: '6umd0gXe3bz13GKGNg0ybfx6XlOhGyLp0IXgVEo',
-  access_token: '26528955-a97TOXHTchFrbmsjH2e8tKaScjbyN7EekHr3zo4ph',
-  access_token_secret: 'ffJR4aThNwg0knbQh9jZstNZCpg4HIsqTd2mgJE'
-});
-
-io.sockets.on('connection', function (socket) {
-
-	(function event_loop(){
-		setTimeout(function(){
-			twit.get('search/tweets', { q: 'india', count: 100 }, function(err, reply) {
-				console.log(err);
-				//var random_number = Math.floor(Math.random()*500)
-				//socket.emit('number', { number: random_number });
-				socket.emit('tweet', { tweet: reply});
-				//event_loop();
-			});
-		}, 10);
-	})();
-
- 	socket.on('my other event', function (data) {
-    	console.log(data);
-  	});
-
-});
 
 
